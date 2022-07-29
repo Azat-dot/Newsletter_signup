82f74a8f18b8f321fa9629d53823cf5573405ab8
azat <azatsafaryan@mail.ru>
2022-07-29 18:47:19 +0500

    preparation done

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
+//jshint esversion: 6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', function(req, res){
-    res.send("Server is runing well")
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post('/', function(req, res){
+
+    const fName = req.body.fName;
+    const lName = req.body.lName;
+    const email = req.body.email;
+    console.log(fName, lName, email);
+
+    res.send()
 })
 
 
 app.listen(3000, function(){
-    console.log("Server is running in host 3000");
-})
+    console.log("Server is running in port 3000");
+});
